d5fd281a0ff5d72908c7b8f7112c7069b679adf2
David <davidmejia.05@hotmail.com>
2026-06-28 12:25:57 -0600

    fix: cors

--- quiniela_live_api/settings.py
+++ quiniela_live_api/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://quiniela-live-api.onrender.com',
+    'https://quina-live-api-dca3ca36bbbb.herokuapp.com',
 ]
 
 # Mercado Pago
