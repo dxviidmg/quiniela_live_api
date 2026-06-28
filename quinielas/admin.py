bd7571748db277566b852960d2063aa4ba58865c
David <davidmejia.05@hotmail.com>
2026-06-28 12:39:05 -0600

    feat(admin): add list_display and list_filter to SeleccionAdmin

--- quinielas/admin.py
+++ quinielas/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Seleccion, Quiniela, Participante
 
-admin.site.register(Seleccion)
+@admin.register(Seleccion)
+class SeleccionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'nombre', 'fase', 'eliminado')
+    list_filter = ('fase', 'eliminado')
 admin.site.register(Quiniela)
 admin.site.register(Participante)
